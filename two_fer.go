9861878938762a1cf19490709ab27dad3f72fccc
Emily Grove <eh.grove@yahoo.com>
2023-05-18 16:55:26 -0500

    Better solution

--- two_fer.go
+++ two_fer.go
@@ -1,15 +1,20 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
+// Package twofer provides a function to generate dialogue for sharing cookies.
+// The ShareWith function determines the appropriate dialogue based on a given name.
 
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package twofer
 
-// ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	//ShareWish returns the appropriate, personalized dialog the user should use for sharing their cookies
+	if name == ""{
+		name = "you"
+	}
+	return "One for " + name + ", one for me."
 }
+
+
+//Write the package comment to summarize what the program is about x
+//write a doc string to summarize the ShareWith function x
+//Create a conditional where if a name is passed in to ShareWith as an argument, 
+	//the return statement will say "One for you {insert name}, one for me". 
+//Otherwise if no name is passed into ShareWith as an argument, if it is an empty string
+	//the return statement will say "One for you, one for me."
